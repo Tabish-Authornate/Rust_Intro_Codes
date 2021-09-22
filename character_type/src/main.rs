64272826ed06894ca17dd4049b57775b009cf873
Tabish <tabish@authornate.com>
2021-09-22 17:32:17 +0500

    first commit

---
+++ character_type/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let c = 'z';
+
+    println!("Value of c: {}", c);
+
+    let z = 'ℤ';
+
+    println!("Value of z: {}", z);
+
+    let heart_eyed_cat = '😻';
+
+    println!("Value of heart_eyed_cat: {}", heart_eyed_cat);
+}
---
+++ numeric_operations/src/main.rs
@@ -0,0 +1,27 @@
+
+fn main() {
+    // addition
+    let sum = 5 + 10;
+
+    println!("Sum: {}", sum);
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    println!("Diffrence: {}", difference);
+
+    // multiplication
+    let product = 4 * 30;
+
+    println!("Product: {}", product);
+
+    // division
+    let quotient = 56.7 / 32.2;
+
+    println!("Quotient: {}", quotient);
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("Remainder: {}", remainder);
+}
